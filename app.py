e37496afb2bff09b88837ce112baa1bb7b1fe47d
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-07-25 02:00:46 +0900

    go to server

---
+++ app.py
@@ -0,0 +1,18 @@
+  
+from flask import Flask , redirect, url_for, request, render_template, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("input.html")
+
+@app.route('/answer', methods = ["POST"])
+def asd():
+    num1 = request.form['num1']
+    num2 = request.form['num2']
+    
+    return render_template("answer.html", answer = int(num1) + int(num2))
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=80, debug= True)
